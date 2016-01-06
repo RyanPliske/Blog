e012e974e48a22c2025367713ac061b9e5124ee5
Ryan Pliske <ryan.pliske@monsanto.com>
2016-01-05 19:27:26 -0600

    Post3 Continued

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ function Setup() {
     $("#sidebar").load("sidebar.html");
     $("#post1").load("post1.html");
     $("#post2").load("post2.html");
+    $("#post3").load("post3.html", function() {
+		$('pre code').each(function(i, block) {
+		    hljs.highlightBlock(block);
+  		});
+	  });
     $("#footer").load("footer.html");
   });
 }
