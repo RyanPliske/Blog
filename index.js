bc337e1e6f405f1c02babf50e0e58471cd877a0f
Ryan Pliske <ryan.pliske@monsanto.com>
2016-01-05 23:50:58 -0600

    some cleanup

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 "use strict";
 
-function Setup() {
+function setup() {
   $( function() { 
     $("#navbar").load("navbar.html");
     $("#blog-header").load("blog-header.html");
     $("#sidebar").load("sidebar.html");
     $("#post1").load("post1.html");
     $("#post2").load("post2.html");
-    $("#post3").load("post3.html", function() {
-		$('pre code').each(function(i, block) {
-		    hljs.highlightBlock(block);
-  		});
-	  });
+    $("#post3").load("post3.html", highlight);
     $("#footer").load("footer.html");
   });
+}
+
+var highlight = function highlight() {
+  hljs.initHighlighting()
 }
