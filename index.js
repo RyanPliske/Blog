4c39a2fc6010a6779a08134d951670994102cdbf
Ryan Pliske <ryan.pliske@monsanto.com>
2016-01-09 09:34:19 -0600

    Began implementing Accordion Widget from JQuery

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function setup() {
+function loadHTML() {
   $( function() { 
     $("#navbar").load("navbar.html");
     $("#blog-header").load("blog-header.html");
@@ -10,8 +10,41 @@ function setup() {
     $("#post3").load("post3.html", highlight);
     $("#footer").load("footer.html");
   });
-}
+};
+
+function prepareAccordion() {
+  $( function() { 
+    $("#accordion").accordion({ 
+      collapsible: true,
+      active: false,
+      heightStyle: "content",
+      beforeActivate: hackAccordion
+    });
+  });
+};
 
 var highlight = function() {
   hljs.initHighlighting()
-}
+};
+
+var hackAccordion = function(event, ui) {
+         // The accordion believes a panel is being opened
+        if (ui.newHeader[0]) {
+            var currHeader  = ui.newHeader;
+            var currContent = currHeader.next('.ui-accordion-content');
+         // The accordion believes a panel is being closed
+        } else {
+            var currHeader  = ui.oldHeader;
+            var currContent = currHeader.next('.ui-accordion-content');
+        }
+        var isPanelSelected = currHeader.attr('aria-selected') == 'true';
+        currHeader.toggleClass('ui-corner-all',isPanelSelected).toggleClass('blog-post-title',!isPanelSelected).attr('aria-selected',((!isPanelSelected).toString()));
+        // currHeader.children('.ui-icon').toggleClass('ui-icon-triangle-1-e',isPanelSelected).toggleClass('ui-icon-triangle-1-s',!isPanelSelected);
+        currContent.toggleClass('blog-post',!isPanelSelected)    
+        if (isPanelSelected) { 
+          currContent.slideUp(); 
+        }  else { 
+          currContent.slideDown(); 
+        }
+        return false; // Cancel the default action
+};
